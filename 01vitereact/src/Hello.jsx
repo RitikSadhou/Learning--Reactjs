06f96c44026bc43662abd3c944cd699e22c5d20c
ritik sadhou <ritiksadhou2000@gmail.com>
2024-01-24 23:07:15 +0530

    completed

---
+++ 01vitereact/src/Hello.jsx
@@ -0,0 +1,6 @@
+function Hello(){
+    return (
+        <h2>Hi Heelo</h2>
+    )
+}
+export default Hello
---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Card from './card'
+import Example from './Example'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myobj={
+
+    username:"ritik",
+    age:23
+
+  }
+
+  return (
+    <>
+    
+      <h1 className='bg-green-500 text-black p-4 rounded-xl mb-4' >Tailwind test</h1>
+      <Card username="chai aur code" btnText="clickme" />
+      <Card username="ritik" btnText="clickme"/>
+    
+  </>
+  ) 
+}
+
+export default App
